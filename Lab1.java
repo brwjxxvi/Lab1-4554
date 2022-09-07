565497d9c17c603115d0a2f6c200ca84a0a8d4e4
Blake Johnson <joh18484@morris.umn.edu>
2022-09-07 14:58:51 -0500

    Added some comments and a method positionToLetter
    positionToLetter changes an integer value back into a character according to the ascii values.

--- Lab1.java
+++ Lab1.java
@@ -9,26 +9,38 @@ public class Lab1 {
 
         for (int i=0; i<phraseLength; i++) {
             ArrayList<Integer> phrasePositions = new ArrayList<Integer>();
-            phrasePositions.add(letterPosition(phrase.charAt(i), 'a'));
+            phrasePositions.add(letterToPosition(phrase.charAt(i), 'a'));
         }
 
         for (int i=0; i<keywordLength; i++) {
             ArrayList<Integer> keywordPositions = new ArrayList<Integer>();
-            keywordPositions.add(letterPosition(keyword.charAt(i), 'a'));
+            keywordPositions.add(letterToPosition(keyword.charAt(i), 'a'));
         }
 
     }
 
-    public static int letterPosition (char x, char a) {
+    // Changes a character to its respective integer ascii value minus
+    // 97 so that a is 0, b is 1, and so on
+    public static int letterToPosition (char x, char a) {
         int position1 = (int) x;
         int position2 = (int) a;
 
         return (position1-position2);
     }
 
+    // Changes an integer back into its respective character value from 97 to 123
+    public static char positionToLetter (int x) {
+        int a = 97;
+        int position = a + x;
+        char letter = (char) position;
+
+        return letter;
+    }
+
     public static void main (String[] args) {
         
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter encrypted text: ");
         String text = input.nextLine().toLowerCase().replaceAll("[^a-zA-Z]","");
 
         System.out.println(text);
